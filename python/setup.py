6abfd3d1af91cdd49111f30d1bce7996776b2435
Justin MacCallum <justin.maccallum@me.com>
2014-07-28 16:31:29 -0600

    Modifications to make make use libc++

--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,29 @@ from distutils.core import setup
 from distutils.extension import Extension
 import os
 import sys
+import platform
 
 openmm_dir = '@OPENMM_DIR@'
 exampleplugin_header_dir = '@EXAMPLEPLUGIN_HEADER_DIR@'
 exampleplugin_library_dir = '@EXAMPLEPLUGIN_LIBRARY_DIR@'
 
+# setup extra compile and link arguments on Mac
+extra_compile_args = []
+extra_link_args = []
+
+openmm_lib_path = os.getenv('OPENMM_LIB_PATH')
+
+if platform.system() == 'Darwin':
+    extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+    extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl', '-rpath', openmm_lib_path]
+
 extension = Extension(name='_exampleplugin',
                       sources=['ExamplePluginWrapper.cpp'],
                       libraries=['OpenMM', 'ExamplePlugin'],
                       include_dirs=[os.path.join(openmm_dir, 'include'), exampleplugin_header_dir],
-                      library_dirs=[os.path.join(openmm_dir, 'lib'), exampleplugin_library_dir]
+                      library_dirs=[os.path.join(openmm_dir, 'lib'), exampleplugin_library_dir],
+                      extra_compile_args=extra_compile_args,
+                      extra_link_args=extra_link_args
                      )
 
 setup(name='exampleplugin',
