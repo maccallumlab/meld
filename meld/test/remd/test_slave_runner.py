65371e2b0c7a15cba17d45c8cdacced74e35cd28
Geoff Rollins <grollins@gmail.com>
2014-08-09 11:34:37 -0700

    Fix tests with out-of-date arguments, call signatures, etc.

--- meld/test/remd/test_slave_runner.py
+++ meld/test/remd/test_slave_runner.py
@@ -41,14 +41,28 @@ class TestSlaveSingle(unittest.TestCase):
             sentinel.STATE_2,
             sentinel.STATE_3,
             sentinel.STATE_4]
+        self.mock_state_1 = mock.Mock()
+        self.mock_state_1.positions = sentinel.pos1
+        self.mock_state_1.velocities = 1.0
+        self.mock_state_2 = mock.Mock()
+        self.mock_state_2.velocities = 1.0
+        self.mock_state_2.positions = sentinel.pos2
+        self.mock_state_3 = mock.Mock()
+        self.mock_state_3.velocities = 1.0
+        self.mock_state_3.positions = sentinel.pos3
+        self.mock_state_4 = mock.Mock()
+        self.mock_state_4.velocities = 1.0
+        self.mock_state_4.positions = sentinel.pos4
+        self.fake_states_after_run = [
+            self.mock_state_1, self.mock_state_2, self.mock_state_3,
+            self.mock_state_4]
+        self.mock_comm.exchange_states_for_energy_calc.return_value = self.fake_states_after_run
 
         self.mock_system_runner = mock.Mock(spec_set=runner.ReplicaRunner)
         self.mock_system_runner.minimize_then_run.return_value = mock.sentinel.STATE
-        self.mock_system_runner.get_energy.side_effect = [
-            sentinel.ENERGY_1,
-            sentinel.ENERGY_2,
-            sentinel.ENERGY_3,
-            sentinel.ENERGY_4]
+        self.FAKE_ENERGIES_AFTER_GET_ENERGY = [
+            sentinel.E1, sentinel.E2, sentinel.E3, sentinel.E4, sentinel.E5, sentinel.E6]
+        self.mock_system_runner.get_energy.side_effect = self.FAKE_ENERGIES_AFTER_GET_ENERGY
 
         self.runner = slave_runner.SlaveReplicaExchangeRunner(step=1, max_steps=1)
 
@@ -76,24 +90,17 @@ class TestSlaveSingle(unittest.TestCase):
 
         self.mock_system_runner.minimize_then_run.assert_called_once_with(mock.sentinel.STATE)
 
-    def test_calls_send_state(self):
-        "should send the state from the system_runner to the master"
-        self.runner.run(self.mock_comm, self.mock_system_runner)
-
-        self.mock_comm.send_state_to_master.assert_called_once_with(mock.sentinel.STATE)
-
-    def test_calls_revieve_all_states(self):
+    def test_calls_exchange_states_for_energy_calc(self):
         "should call receive_states_for_energy_calc from the master"
         self.runner.run(self.mock_comm, self.mock_system_runner)
 
-        self.mock_comm.receive_states_for_energy_calc_from_master.assert_called_once_with()
+        self.mock_comm.exchange_states_for_energy_calc.assert_called_once_with(mock.sentinel.STATE)
 
     def test_calls_get_energy_for_each_state(self):
         "should call get_energy on each state received"
         self.runner.run(self.mock_comm, self.mock_system_runner)
 
-        calls = [mock.call(sentinel.STATE_1), mock.call(sentinel.STATE_2),
-                 mock.call(sentinel.STATE_3), mock.call(sentinel.STATE_4)]
+        calls = [mock.call(s) for s in self.fake_states_after_run]
         self.mock_system_runner.get_energy.assert_has_calls(calls)
 
     def test_sends_energies_back_to_master(self):
@@ -101,7 +108,7 @@ class TestSlaveSingle(unittest.TestCase):
         self.runner.run(self.mock_comm, self.mock_system_runner)
 
         self.mock_comm.send_energies_to_master.assert_called_once_with(
-            [sentinel.ENERGY_1, sentinel.ENERGY_2, sentinel.ENERGY_3, sentinel.ENERGY_4])
+            [sentinel.E1, sentinel.E2, sentinel.E3, sentinel.E4])
 
 
 class TestSlaveMultiple(unittest.TestCase):
@@ -114,7 +121,28 @@ class TestSlaveMultiple(unittest.TestCase):
             sentinel.STATE_2,
             sentinel.STATE_3,
             sentinel.STATE_4]
+        self.mock_state_1 = mock.Mock()
+        self.mock_state_1.positions = sentinel.pos1
+        self.mock_state_1.velocities = 1.0
+        self.mock_state_2 = mock.Mock()
+        self.mock_state_2.velocities = 1.0
+        self.mock_state_2.positions = sentinel.pos2
+        self.mock_state_3 = mock.Mock()
+        self.mock_state_3.velocities = 1.0
+        self.mock_state_3.positions = sentinel.pos3
+        self.mock_state_4 = mock.Mock()
+        self.mock_state_4.velocities = 1.0
+        self.mock_state_4.positions = sentinel.pos4
+        self.fake_states_after_run = [
+            self.mock_state_1, self.mock_state_2, self.mock_state_3,
+            self.mock_state_4]
+        self.mock_comm.exchange_states_for_energy_calc.return_value = self.fake_states_after_run
+
         self.mock_system_runner = mock.Mock(spec_set=runner.ReplicaRunner)
+        self.mock_system_runner.minimize_then_run.return_value = mock.sentinel.STATE
+        self.FAKE_ENERGIES_AFTER_GET_ENERGY = [
+            sentinel.E1, sentinel.E2, sentinel.E3, sentinel.E4] * 4
+        self.mock_system_runner.get_energy.side_effect = self.FAKE_ENERGIES_AFTER_GET_ENERGY
         self.runner = slave_runner.SlaveReplicaExchangeRunner(step=1, max_steps=4)
 
     def test_runs_correct_number_of_steps(self):
