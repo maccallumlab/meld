db493eec80a3df0c1e3f75fb1c49261d1f4fad5b
Justin MacCallum <justin.maccallum@ucalgary.ca>
2014-12-23 17:10:59 -0700

    Fixed small bug in handling of secondary structure.
    The parameter quadratic_cut was incorrectly
    divided by 10 instead of multiplied by 10.
    This would only affect the range over which
    the secondary structure torsions were
    quadratic rather than linear.

--- meld/parse.py
+++ meld/parse.py
@@ -117,7 +117,7 @@ def get_secondary_structure_restraints(system, scaler, ramp=None, torsion_force_
     contents = _get_secondary_sequence(filename, contents, file)
     torsion_force_constant /= 100.
     distance_force_constant *= 100.
-    quadratic_cut /= 10.
+    quadratic_cut *= 10.
 
     groups = []
 
