13a3f4cf4a73e89e1b4bba2f01f5439bc2814d50
AlbertoPerez <aperez@laufer3.cm.cluster>
2015-04-16 11:05:45 -0400

    Added A FixedTemperature ladder where users can specify the exact temperatures to use

--- meld/system/__init__.py
+++ meld/system/__init__.py
@@ -1,6 +1,6 @@
 from .protein import ProteinMoleculeFromSequence, ProteinMoleculeFromPdbFile
 from .builder import SystemBuilder
-from .system import System, ConstantTemperatureScaler, LinearTemperatureScaler
+from .system import System, ConstantTemperatureScaler, LinearTemperatureScaler, FixedTemperatureScaler
 from .system import GeometricTemperatureScaler, RunOptions
 from .runner import get_runner
 from .openmm_runner import OpenMMRunner
