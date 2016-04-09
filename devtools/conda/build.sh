e4bfaf04ad23497047292657d280357b282e4bb2
Justin MacCallum <justin.maccallum@me.com>
2016-04-09 16:21:52 -0600

    Added begginings of conda recipe

---
+++ devtools/conda/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
+
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
+
+CUDA_PATH="/usr/local/cuda-7.5"
+CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=${CUDA_PATH}/lib64/libcudart.so"
+CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=${CUDA_PATH}/bin/nvcc"
+CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
+CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
+CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
+CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
+
+export EIGEN3_INCLUDE_DIR="/anaconda/include/eigen3/"
+export OPENMM_DIR="/anaconda/"
+export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
+export OPENMM_LIB_PATH=$OPENMM_DIR/lib
+export LD_LIBRARY_PATH+=$OPENMM_DIR/lib
+
+mkdir build
+cd build
+
+ls /anaconda/include
+
+cmake .. $CMAKE_FLAGS
+make -j$CPU_COUNT all
+make -j$CPU_COUNT install PythonInstall
--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -15,6 +15,11 @@ curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
-conda install -yq conda-build jinja2 anaconda-client
-conda install -y openmm
+# use Juanlu001 repository until we have an internal
+conda config --add channels Juanlu001
+conda install -yq conda-build jinja2 anaconda-client openmm eigen3
+
+# build the meld conda package
+# copy of eigen3
+conda-build /io/devtools/conda
 
