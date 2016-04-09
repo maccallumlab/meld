32ac739925b1d56e8d3df1c1637ec2914ac2f8aa
Justin MacCallum <justin.maccallum@me.com>
2016-04-09 14:15:58 -0600

    Updated travis

---
+++ devtools/docker-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Activate Holy Build Box environment
+source /hbb_exe/activate
+
+# Disable PYTHONPATH
+unset PYTHONPATH
+
+set -x
+
+# install miniconda
+curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
+PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
+conda config --add channels omnia
+conda install -yq conda-build jinja2 anaconda-client
+
+
+cd /io
+pwd
+ls -lh
