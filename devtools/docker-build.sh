7c3f197fec5f591b8be4e44928a24da115874b85
Justin MacCallum <justin.maccallum@me.com>
2016-04-09 14:29:47 -0600

    Install conda during build.

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -16,8 +16,5 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
+conda install -y openmm
 
-
-cd /io
-pwd
-ls -lh
