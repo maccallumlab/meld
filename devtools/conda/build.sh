4717a53d3c7741a8175068132390608d1404b0b1
Justin MacCallum <justin.maccallum@me.com>
2016-04-12 15:51:37 -0600

    Updated conda packaging

--- devtools/conda/build.sh
+++ devtools/conda/build.sh
@@ -12,11 +12,11 @@ CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
 CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
 CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
 
-export EIGEN3_INCLUDE_DIR="/anaconda/include/eigen3/"
-export OPENMM_DIR="/anaconda/"
+export EIGEN3_INCLUDE_DIR="/anaconda/envs/_build/include/eigen3/"
+export OPENMM_DIR="/anaconda/envs/_build"
 export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
 export OPENMM_LIB_PATH=$OPENMM_DIR/lib
-export LD_LIBRARY_PATH=$OPENMM_DIR/lib
+export LD_LIBRARY_PATH=$OPENMM_DIR/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 
 mkdir build
--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -16,7 +16,6 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
-conda install -yq --channel JuanLu001 eigen3
 
 # build the meld conda package
 conda-build /io/devtools/conda
