a2170e783f5e1ac4957dafbd99dae40d0796e263
Justin MacCallum <justin.maccallum@me.com>
2016-04-28 21:32:10 -0600

    First try at osx build

--- devtools/conda/master/build.sh
+++ devtools/conda/master/build.sh
@@ -4,20 +4,34 @@ CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
 
 CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
-CUDA_PATH="/usr/local/cuda-7.5"
-CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=${CUDA_PATH}/lib64/libcudart.so"
-CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=${CUDA_PATH}/bin/nvcc"
-CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
-CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
-CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
-CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    CUDA_PATH="/usr/local/cuda-7.5"
+    CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=${CUDA_PATH}/lib64/libcudart.so"
+    CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=${CUDA_PATH}/bin/nvcc"
+    CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
+    CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
 
-export EIGEN3_INCLUDE_DIR="/anaconda/envs/_build/include/eigen3/"
-export OPENMM_DIR="/anaconda/envs/_build"
-export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
-export OPENMM_LIB_PATH=$OPENMM_DIR/lib
-export LD_LIBRARY_PATH=$OPENMM_DIR/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+    export EIGEN3_INCLUDE_DIR="/anaconda/envs/_build/include/eigen3/"
+    export OPENMM_DIR="/anaconda/envs/_build"
+    export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
+    export OPENMM_LIB_PATH=$OPENMM_DIR/lib
+    export LD_LIBRARY_PATH=$OPENMM_DIR/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
+    CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=/Developer/NVIDIA/CUDA-7.5"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/CUDA-7.5"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+
+    export EIGEN3_INCLUDE_DIR="/Users/travis/anaconda/envs/_build/include/eigen3/"
+    export OPENMM_DIR="/Users/travis/anaconda/envs/_build"
+    export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
+    export OPENMM_LIB_PATH=$OPENMM_DIR/lib
+    export LD_LIBRARY_PATH=$OPENMM_DIR/lib:$LD_LIBRARY_PATH
+fi
 
 mkdir build
 cd build
@@ -25,4 +39,7 @@ cd build
 cmake .. $CMAKE_FLAGS
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install PythonInstall
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
 make -j$CPU_COUNT DoxygenApiDocs
+fi
