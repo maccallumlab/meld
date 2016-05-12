587c24c2cf5ad5d8078ddd7580be97ae73cf7f75
Justin MacCallum <justin.maccallum@ucalgary.ca>
2016-05-12 11:34:40 -0600

    Revert "Changed cuda paths on osx"
    This reverts commit 70c90afb7936499abfbe06379541a22901863210.

--- devtools/conda/master/build.sh
+++ devtools/conda/master/build.sh
@@ -4,9 +4,8 @@ CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
 
 CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
-CUDA_PATH="/usr/local/cuda-7.5"
-
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    CUDA_PATH="/usr/local/cuda-7.5"
     CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=${CUDA_PATH}/lib64/libcudart.so"
     CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=${CUDA_PATH}/bin/nvcc"
     CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
@@ -23,8 +22,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
     CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
-    CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
-    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
+    CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=/Developer/NVIDIA/CUDA-7.5"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/CUDA-7.5"
     CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
 
     export EIGEN3_INCLUDE_DIR="/Users/travis/anaconda/envs/_build/include/eigen3/"
