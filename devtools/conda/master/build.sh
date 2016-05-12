94bd862d717fa7e858467c4aa44cb0a2303eccd8
Justin MacCallum <justin.maccallum@ucalgary.ca>
2016-05-12 16:16:35 -0600

    Changed linux include path handling in cmake

--- devtools/conda/master/build.sh
+++ devtools/conda/master/build.sh
@@ -6,21 +6,18 @@ CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     CUDA_PATH="/usr/local/cuda-7.5"
+    CMAKE_FLAGS+=" -DEIGEN3_INCLUDE_DIR=/anaconda/envs/_build/include/eigen3/"
     CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=${CUDA_PATH}/lib64/libcudart.so"
     CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=${CUDA_PATH}/bin/nvcc"
     CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=${CUDA_PATH}/"
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=${CUDA_PATH}/include"
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}/"
     CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS_RELEASE=-I/usr/include/nvidia/"
-
-    export EIGEN3_INCLUDE_DIR="/anaconda/envs/_build/include/eigen3/"
-    export OPENMM_DIR="/anaconda/envs/_build"
-    export OPENMM_INCLUDE_PATH=$OPENMM_DIR/include
-    export OPENMM_LIB_PATH=$OPENMM_DIR/lib
+    CMAKE_FLAGS+=" -DOPENMM_DIR=/anaconda/envs/_build"
     export LD_LIBRARY_PATH=$OPENMM_DIR/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export EIGEN3_INCLUDE_DIR="/Users/travis/anaconda/envs/_build/include/eigen3/"
+    CMAKE_FLAGS+=" -DEIGEN3_INCLUDE_DIR=/anaconda/envs/_build/include/eigen3/"
     CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
     CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
     CMAKE_FLAGS+=" -DCUDA_SDK_ROOT_DIR=/Developer/NVIDIA/CUDA-7.5"
