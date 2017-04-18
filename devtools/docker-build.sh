5bbaaa71feacf945565bc58962e98c28d5c188da
Justin MacCallum <justin.maccallum@ucalgary.ca>
2017-04-17 18:19:43 -0600

    Removed awscli dependency

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -8,16 +8,13 @@ source /hbb_exe/activate
 # Disable PYTHONPATH
 unset PYTHONPATH
 
+set -x
 # install miniconda
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
-
-# install aws
-pip install awscli
-yum install -y groff
 
 # get the git revision for the version string
 cd /io
