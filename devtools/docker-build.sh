9b29ac5f398e9b4f50311cb70a7a07cb7a5125df
Justin MacCallum <justin.maccallum@ucalgary.ca>
2018-06-18 16:18:01 -0600

    Updated travis config

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 set -e
-
-# Activate Holy Build Box environment
-source /hbb_exe/activate
-
-# Disable PYTHONPATH
-unset PYTHONPATH
-
 set -x
+
 # install miniconda
-curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
+# curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
+# PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
+conda config --add channels conda-forge
+conda install -yq conda\<=4.3.34
 conda install -yq conda-build=2.1.17 jinja2 anaconda-client
 
 # get the git revision for the version string
