8e20b68ea244346a3cdddb72881d279abd7e5eb8
Christopher Ing <ing.chris@gmail.com>
2018-07-15 23:45:57 -0400

    Added explicit solvent boxes with Builder

--- meld/system/builder.py
+++ meld/system/builder.py
@@ -29,11 +29,17 @@ def load_amber_system(top_filename, crd_filename, patchers=None):
 
 
 class SystemBuilder:
-    def __init__(self, forcefield="ff14sbside", gb_radii="mbondi3"):
+    def __init__(self, forcefield="ff14sbside", gb_radii="mbondi3",
+        explicit_solvent=False, solvent_forcefield="tip3p", solvent_distance=6
+    ):
         self._forcefield = None
         self._set_forcefield(forcefield)
         self._gb_radii = None
         self._set_gb_radii(gb_radii)
+        self._explicit_solvent = explicit_solvent
+        if self._explicit_solvent:
+            self._set_solvent_forcefield(solvent_forcefield)
+            self._solvent_dist = solvent_distance
 
     def build_system_from_molecules(
         self, molecules, patchers=None, leap_header_cmds=None
@@ -55,7 +61,12 @@ class SystemBuilder:
                 mol_ids.append(mol_id)
                 mol.prepare_for_tleap(mol_id)
                 leap_cmds.extend(mol.generate_tleap_input(mol_id))
-            leap_cmds.extend(self._generate_leap_footer(mol_ids))
+            if self._explicit_solvent:
+                leap_cmds.extend(self._generate_solvent(mol_ids))
+                leap_cmds.extend(self._generate_leap_footer([f"solute"]))
+            else:
+                leap_cmds.extend(self._generate_leap_footer(mol_ids))
+
             with open("tleap.in", "w") as tleap_file:
                 tleap_string = "\n".join(leap_cmds)
                 tleap_file.write(tleap_string)
@@ -81,10 +92,42 @@ class SystemBuilder:
         else:
             self._gb_radii = gb_radii
 
+    def _set_solvent_forcefield(self, solvent_forcefield):
+        ff_dict = {
+            "spce": "leaprc.water.spce",
+            "spceb": "leaprc.water.spceb",
+            "opc":  "leaprc.water.opc",
+            "tip3p": "leaprc.water.tip3p",
+            "tip4pew": "leaprc.water.tip4pew",
+        }
+        box_dict = {
+            "spce": "SPCBOX",
+            "spceb": "SPCBOX",
+            "opc":  "OPCBOX",
+            "tip3p": "TIP3PBOX",
+            "tip4pew": "TIP4PEWBOX",
+        }
+        try:
+            self._solvent_forcefield = ff_dict[solvent_forcefield]
+            self._solvent_box = box_dict[solvent_forcefield]
+        except KeyError:
+            raise RuntimeError(f"Unknown solvent_model: {solvent_model}")
+
     def _generate_leap_header(self):
         leap_cmds = []
         leap_cmds.append(f"set default PBradii {self._gb_radii}")
         leap_cmds.append(f"source {self._forcefield}")
+        if self._explicit_solvent:
+            leap_cmds.append(f"source {self._solvent_forcefield}")
+        return leap_cmds
+
+    def _generate_solvent(self, mol_ids):
+        leap_cmds = []
+        list_of_mol_ids = ""
+        for mol_id in mol_ids:
+            list_of_mol_ids += f"{mol_id} "
+        leap_cmds.append(f"solute = combine {{ {list_of_mol_ids} }}")
+        leap_cmds.append(f"solvateBox solute {self._solvent_box} {self._solvent_dist}")
         return leap_cmds
 
     def _generate_leap_footer(self, mol_ids):
