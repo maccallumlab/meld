f1d19ae3b98352d0d22e8d58ff0f2b42c714bebf
Christopher Ing <ing.chris@gmail.com>
2018-07-16 00:40:13 -0400

    Added support for explicit ions with Builder

--- meld/system/builder.py
+++ meld/system/builder.py
@@ -30,16 +30,25 @@ def load_amber_system(top_filename, crd_filename, patchers=None):
 
 class SystemBuilder:
     def __init__(self, forcefield="ff14sbside", gb_radii="mbondi3",
-        explicit_solvent=False, solvent_forcefield="tip3p", solvent_distance=6
+        explicit_solvent=False, solvent_forcefield="tip3p", solvent_distance=6,
+        explicit_ions=False, p_ion="Na+", p_ioncount=0, n_ion="Cl-", n_ioncount=0
     ):
         self._forcefield = None
         self._set_forcefield(forcefield)
         self._gb_radii = None
         self._set_gb_radii(gb_radii)
+
         self._explicit_solvent = explicit_solvent
+        self._explicit_ions = explicit_ions
         if self._explicit_solvent:
             self._set_solvent_forcefield(solvent_forcefield)
             self._solvent_dist = solvent_distance
+
+            if self._explicit_ions:
+                self._set_positive_ion_type(p_ion)
+                self._p_ioncount = p_ioncount
+                self._set_negative_ion_type(n_ion)
+                self._n_ioncount = n_ioncount
 
     def build_system_from_molecules(
         self, molecules, patchers=None, leap_header_cmds=None
@@ -113,6 +122,20 @@ class SystemBuilder:
         except KeyError:
             raise RuntimeError(f"Unknown solvent_model: {solvent_model}")
 
+    def _set_positive_ion_type(self, ion_type):
+        allowed = ["Na+", "K+", "Li+", "Rb+", "Cs+", "Mg+"]
+        if ion_type not in allowed:
+            raise RuntimeError(f"Unknown ion_type: {ion_type}")
+        else:
+            self._p_ion = ion_type
+
+    def _set_negative_ion_type(self, ion_type):
+        allowed = ["Cl-", "I-", "Br-", "F-"]
+        if ion_type not in allowed:
+            raise RuntimeError(f"Unknown ion_type: {ion_type}")
+        else:
+            self._n_ion = ion_type
+
     def _generate_leap_header(self):
         leap_cmds = []
         leap_cmds.append(f"set default PBradii {self._gb_radii}")
@@ -128,6 +151,9 @@ class SystemBuilder:
             list_of_mol_ids += f"{mol_id} "
         leap_cmds.append(f"solute = combine {{ {list_of_mol_ids} }}")
         leap_cmds.append(f"solvateBox solute {self._solvent_box} {self._solvent_dist}")
+        if self._explicit_ions:
+            leap_cmds.append(f"addIons solute {self._p_ion} {self._p_ioncount}")
+            leap_cmds.append(f"addIons solute {self._n_ion} {self._n_ioncount}")
         return leap_cmds
 
     def _generate_leap_footer(self, mol_ids):
