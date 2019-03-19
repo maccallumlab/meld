597c1e9efcb6f041284f8c6705c1b4076b361245
Justin MacCallum <justin.maccallum@ucalgary.ca>
2019-03-19 16:25:23 -0600

    Bump version: 0.4.12 → 0.4.13

--- plugin/python/setup.py
+++ plugin/python/setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 import os
 import platform
 
-version = '0.4.12'
+version = '0.4.13'
 
 openmm_dir = '@OPENMM_DIR@'
 meldplugin_header_dir = '@MELDPLUGIN_HEADER_DIR@'
