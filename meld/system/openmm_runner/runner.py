d54507b7a287303cf3439a2e6db6bc3b919fb77c
Justin MacCallum <justin.maccallum@ucalgary.ca>
2021-05-11 09:08:14 -0600

    Fix typo

--- meld/system/openmm_runner/runner.py
+++ meld/system/openmm_runner/runner.py
@@ -251,7 +251,7 @@ class OpenMMRunner(ReplicaRunner):
                 # We can force the use of nvcc by setting CudaCompiler.
                 # We set it to the default value, which will reflect the
                 # OPENMM_CUDA_COMPILER environmnet variable if set.
-                compiler = platform.getProperyDefaultValue("CudaCompiler")
+                compiler = platform.getPropertyDefaultValue("CudaCompiler")
                 logger.debug(f"Using CUDA compiler {compiler}.")
                 properties = {
                     "CudaDeviceIndex": str(self._device_id),
