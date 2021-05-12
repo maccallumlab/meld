42db271b6c5a828c6a51e806bf0bc1978b35f107
Justin MacCallum <justin.maccallum@ucalgary.ca>
2021-05-12 12:25:55 -0600

    Update CI to build docs

--- docs/conf.py
+++ docs/conf.py
@@ -113,10 +113,7 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ----------------------------------------------
-import msmb_theme
-import sphinx_rtd_theme
-
-html_theme = 'msmb_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,10 +121,7 @@ html_theme = 'msmb_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [
-    msmb_theme.get_html_theme_path(),
-    sphinx_rtd_theme.get_html_theme_path()
-]
+html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
