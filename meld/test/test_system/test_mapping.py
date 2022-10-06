a7d063408dc8941295d5654c80fa1a9abcbbd162
Justin MacCallum <justin.maccallum@ucalgary.ca>
2022-10-06 11:29:38 -0600

    Fixed failing tests

--- meld/test/test_system/test_mapping.py
+++ meld/test/test_system/test_mapping.py
@@ -8,31 +8,31 @@ import numpy as np  # type: ignore
 class TestPeakMapper(unittest.TestCase):
     def test_should_raise_with_negative_n_peeaks(self):
         with self.assertRaises(ValueError):
-            mapping.PeakMapper("test", n_peaks=-1, atom_names=["N", "H"])
+            mapping.PeakMapper("test", n_peaks=-1, n_active=1, atom_names=["N", "H"])
 
     def test_add_atom_group_should_fail_with_too_few(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         with self.assertRaises(KeyError):
             mapper.add_atom_group(N=AtomIndex(0))
 
     def test_add_atom_group_should_fail_with_extra(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         with self.assertRaises(KeyError):
             mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1), Q=AtomIndex(2))
 
     def test_add_atom_group_should_fail_with_mismatch(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         with self.assertRaises(KeyError):
             # no Q
             mapper.add_atom_group(N=AtomIndex(0), Q=AtomIndex(1))
 
     def test_should_raise_with_non_atom_index(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         with self.assertRaises(ValueError):
             mapper.add_atom_group(N=0, H=AtomIndex(1))
 
     def test_atom_groups_length_should_match(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
         mapper.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
         mapper.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
@@ -40,7 +40,7 @@ class TestPeakMapper(unittest.TestCase):
         self.assertEqual(mapper.n_atom_groups, 3)
 
     def test_get_atom_mapping_should_raise_with_bad_peak_id(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
 
         with self.assertRaises(KeyError):
             # no peak_id 10
@@ -51,32 +51,32 @@ class TestPeakMapper(unittest.TestCase):
             mapper.get_mapping(peak_id=-1, atom_name="H")
 
     def test_get_atom_mapping_should_raise_with_name(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
 
         with self.assertRaises(KeyError):
             # no name Q
             mapper.get_mapping(peak_id=0, atom_name="Q")
 
     def test_get_atom_mapping_should_have_correct_name(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         m = mapper.get_mapping(peak_id=0, atom_name="N")
 
         self.assertEqual(m.map_name, "test")
 
     def test_get_atom_mapping_should_have_correct_peak_id(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         m = mapper.get_mapping(peak_id=0, atom_name="N")
 
         self.assertEqual(m.peak_id, 0)
 
     def test_get_atom_mapping_should_have_correct_atom_name(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=10, n_active=10, atom_names=["N", "H"])
         m = mapper.get_mapping(peak_id=0, atom_name="N")
 
         self.assertEqual(m.atom_name, "N")
 
     def test_initial_state_should_have_correct_size(self):
-        mapper1 = mapping.PeakMapper("test", n_peaks=3, atom_names=["N", "H"])
+        mapper1 = mapping.PeakMapper("test", n_peaks=3, n_active=1, atom_names=["N", "H"])
         mapper1.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
 
         # mapper1 has 3 peaks, but 1 atom_group
@@ -84,118 +84,20 @@ class TestPeakMapper(unittest.TestCase):
         # so it should be 3
         self.assertEqual(mapper1.get_initial_state().shape[0], 3)
 
-        mapper2 = mapping.PeakMapper("test", n_peaks=1, atom_names=["N", "H"])
+        mapper2 = mapping.PeakMapper("test", n_peaks=1, n_active=1, atom_names=["N", "H"])
         mapper2.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
         mapper2.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
         mapper2.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
 
         # mapper2 has 1 peak, but 3 atom_group
-        # Initial state should have the size of max of these two,
-        # so it should be 3
-        self.assertEqual(mapper2.get_initial_state().shape[0], 3)
+        # Initial state should have the size of the number of peaks, i.e. 1
+        self.assertEqual(mapper2.get_initial_state().shape[0], 1)
 
-    def test_extract_value_should_fail_with_map_name_mismatch(self):
-        mapper = mapping.PeakMapper("test", n_peaks=10, atom_names=["N", "H"])
-        m = mapping.PeakMapping(map_name="fail", peak_id=0, atom_name="H")
-        state = mapper.get_initial_state()
-
-        with self.assertRaises(KeyError):
-            mapper.extract_value(m, state)
-
-    def test_extract_value_should_give_correct_indices_with_equal_length(self):
-        mapper = mapping.PeakMapper("test", n_peaks=3, atom_names=["N", "H"])
-        mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
-        mapper.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
-        mapper.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
-        state = mapper.get_initial_state()
-
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=0, atom_name="N"), state
-            ),
-            0,
-        )
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=0, atom_name="H"), state
-            ),
-            1,
-        )
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=1, atom_name="N"), state
-            ),
-            2,
-        )
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=1, atom_name="H"), state
-            ),
-            3,
-        )
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=2, atom_name="N"), state
-            ),
-            4,
-        )
-        self.assertEqual(
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=2, atom_name="H"), state
-            ),
-            5,
-        )
-
-    def test_extract_value_should_raise_with_illegal_peak_id(self):
-        mapper = mapping.PeakMapper("test", n_peaks=1, atom_names=["N", "H"])
-        mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
-        mapper.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
-        mapper.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
-        state = mapper.get_initial_state()
-
-        with self.assertRaises(KeyError):
-            mapper.extract_value(
-                mapping.PeakMapping(map_name="test", peak_id=1, atom_name="H"), state
-            )
-
-    def test_extract_value_should_return_not_mapped(self):
-        mapper = mapping.PeakMapper("test", n_peaks=3, atom_names=["N", "H"])
-        mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
-        state = mapper.get_initial_state()
-
-        val1 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=0, atom_name="N"), state
-        )
-        val2 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=0, atom_name="H"), state
-        )
-        val3 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=1, atom_name="N"), state
-        )
-
-        val4 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=1, atom_name="H"), state
-        )
-        val5 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=2, atom_name="N"), state
-        )
-
-        val6 = mapper.extract_value(
-            mapping.PeakMapping(map_name="test", peak_id=2, atom_name="H"), state
-        )
-
-        # We have 3 peaks, but only one atom group, so only one peak will
-        # map to something. With the default initialization, this will be
-        # the first peak.
-        self.assertEqual(val1, 0)
-        self.assertEqual(val2, 1)
-        self.assertIsInstance(val3, mapping.NotMapped)
-        self.assertIsInstance(val4, mapping.NotMapped)
-        self.assertIsInstance(val5, mapping.NotMapped)
-        self.assertIsInstance(val6, mapping.NotMapped)
 
     def test_sample_has_correct_shape(self):
-        mapper = mapping.PeakMapper("test", n_peaks=3, atom_names=["N", "H"])
+        mapper = mapping.PeakMapper("test", n_peaks=3, n_active=1, atom_names=["N", "H"])
+        mapper.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
+        mapper.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
         state = mapper.get_initial_state()
 
         trial_state = mapper.sample(state)
@@ -207,13 +109,13 @@ class TestPeakMapManager(unittest.TestCase):
     def test_should_raise_if_duplicate_name(self):
         manager = mapping.PeakMapManager()
         with self.assertRaises(ValueError):
-            manager.add_map("test", 100, ["N", "H"])
-            manager.add_map("test", 100, ["N", "H"])
+            manager.add_map("test", 100, 100, ["N", "H"])
+            manager.add_map("test", 100, 100, ["N", "H"])
 
     def test_get_mapping_from_multiple_mappers(self):
         manager = mapping.PeakMapManager()
-        map1 = manager.add_map("test1", 100, ["N", "H"])
-        map2 = manager.add_map("test2", 100, ["N", "H"])
+        map1 = manager.add_map("test1", 100, 1, ["N", "H"])
+        map2 = manager.add_map("test2", 100, 1, ["N", "H"])
 
         m1 = map1.get_mapping(peak_id=0, atom_name="H")
         m2 = map2.get_mapping(peak_id=1, atom_name="N")
@@ -229,11 +131,11 @@ class TestPeakMapManager(unittest.TestCase):
         manager = mapping.PeakMapManager()
 
         # map1 has initial state of size 3
-        map1 = manager.add_map("test1", 3, ["h"])
+        map1 = manager.add_map("test1", 3, 1, ["h"])
         map1.add_atom_group(h=AtomIndex(0))
 
         # map2 has initial state of size 2
-        map1 = manager.add_map("test2", 1, ["h"])
+        map1 = manager.add_map("test2", 2, 2, ["h"])
         map1.add_atom_group(h=AtomIndex(0))
         map1.add_atom_group(h=AtomIndex(1))
 
@@ -245,11 +147,11 @@ class TestPeakMapManager(unittest.TestCase):
         manager = mapping.PeakMapManager()
 
         # map1 has initial state of size 3
-        map1 = manager.add_map("test1", 3, ["h"])
+        map1 = manager.add_map("test1", 3, 1, ["h"])
         map1.add_atom_group(h=AtomIndex(0))
 
         # map2 has initial state of size 2
-        map1 = manager.add_map("test2", 1, ["h"])
+        map1 = manager.add_map("test2", 1, 1, ["h"])
         map1.add_atom_group(h=AtomIndex(0))
         map1.add_atom_group(h=AtomIndex(1))
 
@@ -257,10 +159,9 @@ class TestPeakMapManager(unittest.TestCase):
         # so 0, 1, 2, 0, 1
         init = manager.get_initial_state()
         self.assertEqual(init[0], 0)
-        self.assertEqual(init[1], 1)
-        self.assertEqual(init[2], 2)
+        self.assertEqual(init[1], -1)
+        self.assertEqual(init[2], -1)
         self.assertEqual(init[3], 0)
-        self.assertEqual(init[4], 1)
 
     def test_initial_state_should_handle_case_when_empty(self):
         manager = mapping.PeakMapManager()
@@ -273,12 +174,12 @@ class TestPeakMapManager(unittest.TestCase):
         manager = mapping.PeakMapManager()
 
         # map1 has 2 pekas, 2 atom_groups
-        map1 = manager.add_map("map1", 2, atom_names=["N", "H"])
+        map1 = manager.add_map("map1", 2, 2, atom_names=["N", "H"])
         map1.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
         map1.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
 
         # map2 has 2 peaks, but only 1 atom_group -> 1 peak will be NotMapped
-        map2 = manager.add_map("map2", 2, atom_names=["N", "H"])
+        map2 = manager.add_map("map2", 2, 1, atom_names=["N", "H"])
         map2.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
 
         state = manager.get_initial_state()
@@ -298,13 +199,19 @@ class TestPeakMapManager(unittest.TestCase):
         self.assertEqual(ind4, 3)
         self.assertEqual(ind5, 4)
         self.assertEqual(ind6, 5)
-        self.assertIsInstance(ind7, mapping.NotMapped)
-        self.assertIsInstance(ind8, mapping.NotMapped)
+        self.assertEqual(ind7, -1)
+        self.assertEqual(ind8, -1)
 
     def test_sample_has_correct_shape(self):
         manager = mapping.PeakMapManager()
-        manager.add_map("map1", n_peaks=3, atom_names=["N", "H"])
-        manager.add_map("map2", n_peaks=3, atom_names=["N", "H"])
+        map1 = manager.add_map("map1", n_peaks=3, n_active=3, atom_names=["N", "H"])
+        map1.add_atom_group(N=AtomIndex(0), H=AtomIndex(1))
+        map1.add_atom_group(N=AtomIndex(2), H=AtomIndex(3))
+        map1.add_atom_group(N=AtomIndex(4), H=AtomIndex(5))
+        map2 = manager.add_map("map2", n_peaks=3, n_active=3, atom_names=["N", "H"])
+        map2.add_atom_group(N=AtomIndex(6), H=AtomIndex(7))
+        map2.add_atom_group(N=AtomIndex(8), H=AtomIndex(9))
+        map2.add_atom_group(N=AtomIndex(10), H=AtomIndex(11))
 
         state = manager.get_initial_state()
 
