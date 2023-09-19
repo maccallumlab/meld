c2b04f41bb8950588cda9606e2b4d7e955de6bdd
splenz <stefan.lenz@ucalgary.ca>
2023-09-19 15:19:41 -0600

    Fixed logic in run of SimulatedAnnealingRunner

--- meld/sa/runner.py
+++ meld/sa/runner.py
@@ -32,13 +32,6 @@ class SimulatedAnnealingRunner:
         """
         return self._timesteps_per_alpha * len(self._alphas)
 
-    @property
-    def timesteps_per_alpha(self) -> int:
-        """
-        number of alpha steps
-        """
-        return self._timesteps_per_alpha
-
     _alphas: List[float]
 
     def __init__(
@@ -46,7 +39,6 @@ class SimulatedAnnealingRunner:
         n_alpha_steps,
         system: meld_system.System,
         runner: interfaces.IRunner,
-        options: options.RunOptions,
         platform: Optional[str] = None,
     ) -> None:
         """
@@ -60,8 +52,6 @@ class SimulatedAnnealingRunner:
         """
         self.id = np.random.randint(0x7FFFFFFF)
         self._n_alpha_steps = n_alpha_steps
-        self.options = options
-        self._timesteps_per_alpha = self.options.timesteps
         self.system = system
         self.platform = platform
         self.runner = runner
@@ -71,45 +61,50 @@ class SimulatedAnnealingRunner:
         self,
     ):
         state = self.system.get_state_template()
+        self._step = 0
 
-        energy = 0
-        for idx, alpha in enumerate(self._alphas):
-            state.alpha = alpha
-            self._step = 1
+        for alpha1, alpha2 in zip(self._alphas[0:-1], self._alphas[1:]):
+            # Prepare system for first alpha and run - keep track of energy
+            self.runner.prepare_for_timestep(state, alpha1, self._step)
+            state.alpha = alpha1
+            state = self.runner.run(state)
+            energy1 = self._compute_energy(state)
 
-            while self._step <= self.timesteps_per_alpha:
-                self.runner.prepare_for_timestep(state, alpha, self._step)
+            # This is a bit of unnecessary overhead because we will redo this at the start of next looop
+            # but makes calculating work easier
+            # update state with new alpha - keep same mappings/coordinates
+            state.alpha = alpha2
+            self.runner.prepare_for_timestep(state, alpha2, self._step)
 
-                if self._step == 1:
-                    energy2 = self._compute_and_output_energy(state)
-                    work = self._compute_and_output_work(state, energy, energy2)
+            # Compute energy after alpha value is changed and state is updated.
+            energy2 = self._compute_energy(state)
+            work = energy2 - energy1
 
+            self._output_energy(alpha1, energy1)
+            self._output_work(alpha2, work)
+
+            # If we are on the last loop, we need to run the last alpha value
+            # This will not have a work value associated because we there is no next alpha value
+            # So we run and compute/save energy
+            if alpha2 == self.alphas[-1]:
                 state = self.runner.run(state)
-                self._step = self._step + 1
+                last_energy = self._compute_energy(state)
+                self._output_energy(alpha2, last_energy)
 
-            energy = self._compute_and_output_energy(state)
-            if idx < len(self._alphas) - 1:
-                work = self._compute_and_output_work(
-                    state, self.alphas[idx], self.alphas[idx + 1]
-                )
-
-        np.save(f"mappings_{self.id}.npy", state.mappings)
+        np.save(f"log_mappings_{self.id}.npy", state.mappings)
 
     def _setup_alphas(self) -> None:
         delta = 1.0 / (self._n_alpha_steps - 1.0)
         self._alphas = [i * delta for i in range(self._n_alpha_steps)]
         self.alphas.reverse()
 
-    def _compute_and_output_energy(self, state) -> float:
-        energy = self.runner.get_energy(state)
-        with open(f"energy_{self.id}.txt", "a+") as energyfile:
-            energyfile.write(f"{state.alpha} {energy} \n")
-        return energy
+    def _compute_energy(self, state) -> float:
+        return self.runner.get_energy(state)
 
-    def _compute_and_output_work(self, state, energy1, energy2) -> float:
-        work = energy2 - energy1
+    def _output_energy(self, alpha, energy) -> None:
+        with open(f"log_energy_{self.id}.txt", "a+") as energyfile:
+            energyfile.write(f"{alpha} {energy} \n")
 
-        with open(f"work_{self.id}.txt", "a+") as wfile:
-            wfile.write(f"{state.alpha} {work} \n")
-
-        return work
+    def _output_work(self, alpha, work) -> None:
+        with open(f"log_work_{self.id}.txt", "a+") as wfile:
+            wfile.write(f"{alpha} {work} \n")
